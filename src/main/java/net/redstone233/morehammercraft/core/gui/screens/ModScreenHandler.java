068fe2bb4d4b5e861c42346ebefa1ded4fbc4264
Redstone2337 <3356168312@qq.com>
2024-12-10 23:40:10 +0800

    Update ModScreenHandler.java

--- src/main/java/net/redstone233/morehammercraft/core/gui/screens/ModScreenHandler.java
+++ src/main/java/net/redstone233/morehammercraft/core/gui/screens/ModScreenHandler.java
@@ -11,7 +11,7 @@ import net.redstone233.morehammercraft.data.PolishMachineData;
 public class ModScreenHandler {
     public static final ScreenHandlerType<PolishingMachineScreenHandler> POLISHING_MACHINE_SCREEN_HANDLER_SCREEN_HANDLER_TYPE =
             /*Registry.register(Registries.SCREEN_HANDLER, Identifier.of(MoreHammerCraft.MOD_ID,"conversion_table"),*/
-                    new ExtendedScreenHandlerType<>(PolishingMachineScreenHandler::new, PolishMachineData.CODEC));
+                    new ExtendedScreenHandlerType<>(PolishingMachineScreenHandler::new, PolishMachineData.CODEC);
 
 
     public static void register() {
