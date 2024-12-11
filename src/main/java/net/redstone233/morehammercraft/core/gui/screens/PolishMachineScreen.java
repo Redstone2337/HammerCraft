a88fc44d00c9d5e248d555a6cc24d5c577d66072
Redstone2337 <3356168312@qq.com>
2024-12-11 09:45:19 +0800

    Update PolishMachineScreen.java

--- src/main/java/net/redstone233/morehammercraft/core/gui/screens/PolishMachineScreen.java
+++ src/main/java/net/redstone233/morehammercraft/core/gui/screens/PolishMachineScreen.java
@@ -15,6 +15,7 @@ import java.util.function.Function;
 
 public class PolishMachineScreen extends HandledScreen<PolishingMachineScreenHandler> {
     private static final Identifier TEXTURE = Identifier.of(MoreHammerCraft.MOD_ID,"textures/gui/conversion_table.png");
+    private static final Identifier TEXTURE_PROGRESS = Identifier.of(MoreHammerCraft.MOD_ID,"textures/gui/conversion_table.png");
     
     public PolishMachineScreen(PolishingMachineScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
@@ -24,14 +25,14 @@ public class PolishMachineScreen extends HandledScreen<PolishingMachineScreenHan
     protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
         int x = (this.width - this.backgroundWidth) / 2;
         int y = (this.height - this.backgroundHeight) / 2;
-        context.drawTexture(RenderLayer::getGuiTextured,TEXTURE,x,y,0,0,backgroundWidth,backgroundHeight,256,256);
+        context.drawTexture(TEXTURE,x,y,0,0,this.backgroundWidth,this.backgroundHeight);
         renderProgressArrow(context,x,y);
     }
 
     private void renderProgressArrow(DrawContext context, int x, int y) {
         if (handler.isCrafting() && handler.isRaining()) {
-            context.drawTexture(RenderLayer::getGuiTextured,TEXTURE,x + 85,y + 30,176,0,8,handler.getScaledProgress(),backgroundWidth,backgroundHeight);
-
+            //context.drawTexture(RenderLayer::getGuiTextured,TEXTURE,x + 85,y + 30,176,0,8,handler.getScaledProgress(),backgroundWidth,backgroundHeight);
+            context.drawTexture(TEXTURE_PROGRESS,x + 85,y + 30,176,0,8,handler.getScaledProgress(),this.backgroundWidth,this.backgroundHeight);
         }
     }
 }
