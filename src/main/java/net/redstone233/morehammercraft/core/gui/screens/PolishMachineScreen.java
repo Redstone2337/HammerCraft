8fb020fa641855618d1b7b13f67f0a24f83cc5f9
Redstone2337 <3356168312@qq.com>
2024-12-11 10:20:36 +0800

    Update PolishMachineScreen.java

--- src/main/java/net/redstone233/morehammercraft/core/gui/screens/PolishMachineScreen.java
+++ src/main/java/net/redstone233/morehammercraft/core/gui/screens/PolishMachineScreen.java
@@ -1,8 +1,6 @@
 package net.redstone233.morehammercraft.core.gui.screens;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.GameRenderer;
@@ -15,10 +13,9 @@ import net.redstone233.morehammercraft.MoreHammerCraft;
 
 import java.util.function.Function;
 
-@Environment(EnvType.CLIENT)
+
 public class PolishMachineScreen extends HandledScreen<PolishingMachineScreenHandler> {
     private static final Identifier TEXTURE = Identifier.of(MoreHammerCraft.MOD_ID,"textures/gui/conversion_table.png");
-    private static final Identifier TEXTURE_PROGRESS = Identifier.of(MoreHammerCraft.MOD_ID,"textures/gui/conversion_table.png");
     
     public PolishMachineScreen(PolishingMachineScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
@@ -28,15 +25,15 @@ public class PolishMachineScreen extends HandledScreen<PolishingMachineScreenHan
     protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
         int x = (this.width - this.backgroundWidth) / 2;
         int y = (this.height - this.backgroundHeight) / 2;
-        //context.drawTexture(RenderLayer::getGuiTextured,TEXTURE,x + 85,y + 30,176,0,8,handler.getScaledProgress(),backgroundWidth,backgroundHeight);
-        context.drawTexture(TEXTURE,x,y,0,0,this.backgroundWidth,this.backgroundHeight);
+        //context.drawTexture(RenderLayer::getGuiTextured,TEXTURE,x,y,0,0,backgroundWidth,backgroundHeight,256,256);
+        context.drawGuiTexture(TEXTURE,x,y,0,0,this.backgroundWidth,this.backgroundHeight);
         renderProgressArrow(context,x,y);
     }
 
     private void renderProgressArrow(DrawContext context, int x, int y) {
         if (handler.isCrafting() && handler.isRaining()) {
             //context.drawTexture(RenderLayer::getGuiTextured,TEXTURE,x + 85,y + 30,176,0,8,handler.getScaledProgress(),backgroundWidth,backgroundHeight);
-            context.drawTexture(TEXTURE,x + 85,y + 30,176,0,8,handler.getScaledProgress(),this.backgroundWidth,this.backgroundHeight);
+            context.drawGuiTexture(TEXTURE,x + 85,y + 30,176,0,8,handler.getScaledProgress(),this.backgroundWidth,this.backgroundHeight);
         }
     }
 }
