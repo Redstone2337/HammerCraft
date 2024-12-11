5717f305b61e04cea61fc28cd76d5a12aefd26d0
Redstone2337 <3356168312@qq.com>
2024-12-11 09:58:01 +0800

    Update PolishMachineScreen.java

--- src/main/java/net/redstone233/morehammercraft/core/gui/screens/PolishMachineScreen.java
+++ src/main/java/net/redstone233/morehammercraft/core/gui/screens/PolishMachineScreen.java
@@ -13,6 +13,7 @@ import net.redstone233.morehammercraft.MoreHammerCraft;
 
 import java.util.function.Function;
 
+@Environment(EnvType.CLIENT)
 public class PolishMachineScreen extends HandledScreen<PolishingMachineScreenHandler> {
     private static final Identifier TEXTURE = Identifier.of(MoreHammerCraft.MOD_ID,"textures/gui/conversion_table.png");
     private static final Identifier TEXTURE_PROGRESS = Identifier.of(MoreHammerCraft.MOD_ID,"textures/gui/conversion_table.png");
@@ -25,6 +26,7 @@ public class PolishMachineScreen extends HandledScreen<PolishingMachineScreenHan
     protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
         int x = (this.width - this.backgroundWidth) / 2;
         int y = (this.height - this.backgroundHeight) / 2;
+        //context.drawTexture(RenderLayer::getGuiTextured,TEXTURE,x + 85,y + 30,176,0,8,handler.getScaledProgress(),backgroundWidth,backgroundHeight);
         context.drawTexture(TEXTURE,x,y,0,0,this.backgroundWidth,this.backgroundHeight);
         renderProgressArrow(context,x,y);
     }
@@ -32,7 +34,7 @@ public class PolishMachineScreen extends HandledScreen<PolishingMachineScreenHan
     private void renderProgressArrow(DrawContext context, int x, int y) {
         if (handler.isCrafting() && handler.isRaining()) {
             //context.drawTexture(RenderLayer::getGuiTextured,TEXTURE,x + 85,y + 30,176,0,8,handler.getScaledProgress(),backgroundWidth,backgroundHeight);
-            context.drawTexture(TEXTURE_PROGRESS,x + 85,y + 30,176,0,8,handler.getScaledProgress(),this.backgroundWidth,this.backgroundHeight);
+            context.drawTexture(TEXTURE,x + 85,y + 30,176,0,8,handler.getScaledProgress(),this.backgroundWidth,this.backgroundHeight);
         }
     }
 }
