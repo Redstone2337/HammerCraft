6da7f417fcad99f62801dff1a5b357ef69aa6440
Redstone2337 <3356168312@qq.com>
2024-12-12 21:47:41 +0800

    Add files via upload

--- src/main/java/net/redstone233/morehammercraft/MoreHammerCraftClient.java
+++ src/main/java/net/redstone233/morehammercraft/MoreHammerCraftClient.java
@@ -2,7 +2,10 @@ package net.redstone233.morehammercraft;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.util.Identifier;
+import net.redstone233.morehammercraft.core.gui.screens.ModScreenHandler;
+import net.redstone233.morehammercraft.core.gui.screens.PolishMachineScreen;
 import net.redstone233.morehammercraft.items.ModItems;
 
 
@@ -13,6 +16,9 @@ public class MoreHammerCraftClient implements ClientModInitializer {
         FuelRegistryEvents.BUILD.register(Identifier.of(MoreHammerCraft.MOD_ID,"flydragon"),(builder, context) -> {
             builder.add(ModItems.FLYDRAGON,30000);
         });
-        MoreHammerCraft.LOGGER.info("燃料注册成功！");
+
+        HandledScreens.register(ModScreenHandler.POLISHING_MACHINE_SCREEN_HANDLER_SCREEN_HANDLER_TYPE, PolishMachineScreen::new);
+
+        MoreHammerCraft.LOGGER.info(MoreHammerCraft.MOD_ID+"的燃料注册成功！");
     }
 }
