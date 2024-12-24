f55f1694d34d49fc5bc3a0fe44c980ff3f2fedb8
Redstone2337 <3356168312@qq.com>
2024-12-24 21:58:50 +0800

    Add files via upload

--- src/main/java/net/redstone233/morehammercraft/tags/ModBlockTags.java
+++ src/main/java/net/redstone233/morehammercraft/tags/ModBlockTags.java
@@ -9,12 +9,14 @@ import net.redstone233.morehammercraft.MoreHammerCraft;
 public class ModBlockTags {
 
     public static final TagKey<Block> ORE_LIST = of("ore_list");
+    public static final TagKey<Block> BLOCK_LIST = of("mod_block_list");
+    public static final TagKey<Block> RUBY_ORES = of("ruby_ores");
 
     private static TagKey<Block> of(String id) {
         return TagKey.of(RegistryKeys.BLOCK, Identifier.of(MoreHammerCraft.MOD_ID,id));
     }
 
     public static void registerModBlockTags(){
-
+        MoreHammerCraft.LOGGER.info("Registering ModBlockTags");
     }
 }
--- src/main/java/net/redstone233/morehammercraft/tags/ModItemTags.java
+++ src/main/java/net/redstone233/morehammercraft/tags/ModItemTags.java
@@ -11,12 +11,14 @@ public class ModItemTags {
     public static final TagKey<Item> TEMPLATE_LIST = of("template_list");
     public static final TagKey<Item> HAMMER_LIST = of("hammer_list");
     public static final TagKey<Item> INGOT_LIST = of("ingot_list");
+    public static final TagKey<Item> FUNCTION_LIST = of("function_list");
+    public static final TagKey<Item> SICKLE_LIST = of("sickle_list");
 
     private static TagKey<Item> of(String id) {
         return TagKey.of(RegistryKeys.ITEM, Identifier.of(MoreHammerCraft.MOD_ID,id));
     }
 
     public static void registerModItemTags() {
-
+        MoreHammerCraft.LOGGER.info("Registering mod item tags");
     }
 }
